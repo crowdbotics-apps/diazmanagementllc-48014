37658782bd4521e726831a23c1b02c2f0f20518c
Crowdbotics <team@crowdbotics.com>
2024-05-18 03:25:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "diazmanagementllc-48014",
+  url: "https://diazmanagementllc-48014.botics.co",
+  api: "https://diazmanagementllc-48014.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
